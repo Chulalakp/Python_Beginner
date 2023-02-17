53b060306dfba2222f227f9253d49ad6d57bb4f8
Chulalakp <125630521+Chulalakp@users.noreply.github.com>
2023-02-17 19:00:32 +0800

    Add files via upload

---
+++ tao.py
@@ -0,0 +1,12 @@
+import turtle
+tao = turtle.Pen() #first letter of pen  must spell with uppercase
+tao.shape('turtle')
+
+for i in range(18): #drawing command
+    tao.fd(100)
+    tao.left(100)
+
+tao.stamp # leavin stamp
+tao.pos #checkin current position
+tao.goto #move to the certain point
+ 
